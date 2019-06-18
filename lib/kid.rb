6256671c3d84ebcf28859ffbe8b878857e079816
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-06-18 02:09:18 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -9,11 +9,11 @@ class Kid
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 
-  # attr_accessor :name 
+  attr_accessor :name 
   
-  # def initialize(name)
-  #   @name = name 
-  # end 
+  def initialize(name)
+    @name = name 
+  end 
   
   
 end 
