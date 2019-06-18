458afe9805b65a73153d995971bb7f75a3d57a33
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-06-18 02:08:15 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -6,10 +6,8 @@ class Kid
   # include Dance 
   # extend MetaDancing 
   
-  extend 
-  FancyDance::ClassMethods
-  include 
-  FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 
   attr_accessor :name 
   
