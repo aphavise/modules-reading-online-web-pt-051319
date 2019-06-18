257d41c6b6b1d67b38461f5f42e41f09c84c81ee
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-06-18 02:03:59 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,24 +4,21 @@ require_relative './fancy_Dance.rb'
 
 
 class Dancer 
-  include Dance 
-  extend MetaDancing 
+  # include Dance 
+  # extend MetaDancing 
   
   extend
   FancyDance::ClassMethods
   
   include
   FancyDance::InstanceMethods
-end 
-  
+
   
   attr_accessor :name 
   
   def initialize(name)
     @name = name 
   end 
-  
 
-  
   
 end 
